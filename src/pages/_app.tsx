6c5a9c3a7a05201792eb3ebe8c9289e1ed694ec7
Leonardo <leonardoloda2@gmail.com>
2023-12-03 20:53:04 -0500

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <title>WatchLog - Your Movie, Series, and Anime Tracker</title>
+      <meta name="description" content="Keep a record of all the movies, animes and series you've seen." />
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="description"
+        content="Keep track of all the movies, series, and animes you've watched with WatchLog. A simple and efficient way to manage your entertainment history."
+      />
+      <meta name="keywords" content="watchlog, movie tracker, series tracker, anime tracker, watch history" />
+      <meta name="author" content="Leo" />
+      <Component {...pageProps} />
+    </>
+  );
 }
